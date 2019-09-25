5f4ea7a57173d4af35a5f9582c4ec559af043f96
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 22:55:15 +0700

    Add files via upload

---
+++ src/latihan10/logika/Latihan10Logika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan10.logika;
+
+/**
+ *
+ * @author Aero
+ * NAMA     : Irman Novryansah
+ * KELAS    : IF-1
+ * NIM      : 10118018
+ * Deskripsi Program : Program ini untum menampilkan Logika dari tipe data boolen
+ */
+public class Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+       int umur = 19;
+       boolean  dewasa;
+       dewasa = (umur >= 17);
+       
+       //TampilOutput
+        System.out.println("Status Dewasa : "+ dewasa);
+            
+        }
+    }
+    
+
